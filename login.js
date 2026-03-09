99c1fa028d40716ed78b6d568a7b48b060d3d907
josephinekwembe28-oss <josephinekwembe28@gmail.com>
2026-03-09 12:01:28 -0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,9 @@
+const game Text = document.getElementByid('game=text2');
+const userInput = document.getElementByid('user-input2');
+onst submitBtn  = document.getElementByid('submit-btn2');
+function print(text) {
+  gameText.innerText+"\n";
+  gameText.scrollTop = gameText.scrollHeight;
+}
+
+
